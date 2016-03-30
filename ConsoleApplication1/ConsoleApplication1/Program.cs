b9424d1a7605e51b1afdd9f559e8315c006e934d
kdani123 <karpatidaniel.bce@gmail.com>
2016-03-30 21:34:51 +0200

    New2

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, World Again!");
+            System.Console.WriteLine("Hello, World Again2!");
         }
     }
 }
